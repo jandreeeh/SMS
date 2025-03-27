48f9bf9b5191125385cf5a42c5d6cedeec6cf924
Jandre <leejohnandrei68@gmail.com>
2025-03-27 14:19:29 +0800

    Added SMS.Infrastructure

---
+++ SMS.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SMS.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
