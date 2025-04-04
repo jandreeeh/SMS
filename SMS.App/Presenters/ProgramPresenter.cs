e779e29ee24c953acb05659c60899c902b8987b7
Jandre <leejohnandrei68@gmail.com>
2025-04-04 15:21:07 +0800

    set up ProgramPresenter at Program.cs

--- SMS.App/Presenters/ProgramPresenter.cs
+++ SMS.App/Presenters/ProgramPresenter.cs
@@ -1,4 +1,6 @@
 ﻿using SMS.App.Views.IViews;
+using SMS.Domain;
+using SMS.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,28 +12,52 @@ namespace SMS.App.Presenters
     public class ProgramPresenter
     {
         private IProgramView _programView;
+        private AppDbContext _dbContext;
+        private IEnumerable<Programs> _programList;
+        private BindingSource _bindingSource;
 
         public ProgramPresenter(IProgramView programView)
         {
             _programView = programView;
+            _dbContext = new AppDbContext();
+            _bindingSource = new BindingSource();
 
             _programView.CreateEvent += CreateEvent;
             _programView.ReadEvent += ReadEvent;
             _programView.UpdateEvent += UpdateEvent;
             _programView.DeleteEvent += DeleteEvent;
+            LoadProgramList();
+            _programView.GetProgramList(_bindingSource);
+        }
+
+        private void LoadProgramList()
+        {
+            _programList = _dbContext.Programs.ToList();
+            _bindingSource.DataSource = _programList;
         }
 
         private void CreateEvent(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            var program = new Programs
+            {
+                ProgramName = _programView.ProgramName,
+                Description = _programView.Description
+            };
+            _dbContext.Programs.Add(program);
+            _dbContext.SaveChanges();
+            LoadProgramList();
         }
+
         private void ReadEvent(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            LoadProgramList();
+        }
+
         private void UpdateEvent(object? sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
+
         private void DeleteEvent(object? sender, EventArgs e)
         {
             throw new NotImplementedException();
